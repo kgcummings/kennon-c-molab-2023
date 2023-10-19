198cace06ccc85f5870aa1013e6329dcd492d4a9
kgcummings <113394583+kgcummings@users.noreply.github.com>
2023-10-18 21:48:33 -0400

    Week 6 Button Tweaks

--- WEEK 4/WEEK 4/PlayingAudio.swift
+++ WEEK 4/WEEK 4/PlayingAudio.swift
@@ -33,79 +33,101 @@ struct PlayingAudio: View {
                 Text("THE MUSIC ZONE")
                     .font(.system(size: 36).weight(.bold))
                     .multilineTextAlignment(.center)
-                Image(systemName: "pianokeys")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width:100, height:100)
-                    
-                
-                Button("Piano Music") {
+// BUTTON ZONE
+// REFERENCE: https://sarunw.com/posts/swiftui-image-button/ , WEEK 6 UPDATE: ADDED IMAGES TO LINK INSTEAD OF BEING STAGNANT
+                Button(action: {
                     player?.stop()
                     navPath.append( "piano" )
                     player = playTheTunes("PIANO.wav")
                     player?.play()
-    }
-                Image(systemName: "exclamationmark.bubble.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width:100, height:100)
-                
-                Button("Confused sound") {
+                })  {
+                    Image(systemName: "pianokeys")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width:100, height:100)
+                        .foregroundColor(Color.white)
+                        .accentColor(.black)
+                   Text("Piano Keys")
+                }
+                    
+                Button(action: {
                     player?.stop()
                     navPath.append( "confusion" )
                     player = playTheTunes("SOUND1.wav")
                     player?.play()
+                })  {
+                    Image(systemName: "exclamationmark.bubble.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width:100, height:100)
+                        .foregroundColor(Color.white)
+                        .accentColor(.black)
+                   Text("Confusion")
                 }
-                Image(systemName: "cat")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width:100, height:100)
                     
-                
-                Button("Cat Meow") {
+                Button(action: {
                     player?.stop()
                     navPath.append( "a cat" )
                     player = playTheTunes("meow.wav")
                     player?.play()
-    }
-                Image(systemName: "cloud.bolt.rain")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width:100, height:100)
-                    
+                })  {
+                    Image(systemName: "cat")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width:100, height:100)
+                        .foregroundColor(Color.white)
+                        .accentColor(.black)
+                   Text("Cat's Meow")
+                }
+                 
                 
-                Button("Evening Storm") {
+                Button(action: {
                     player?.stop()
                     navPath.append( "a calming storm" )
                     player = playTheTunes("RAIN.wav")
                     player?.play()
-    }
+                })  {
+                    Image(systemName: "cloud.bolt.rain")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width:100, height:100)
+                        .foregroundColor(Color.white)
+                        .accentColor(.black)
+                   Text("Evening Storm")
+                }
                 Spacer()
-            }
-            
-          //  .navigationTitle("The Music App")
-            .background(Color.blue)
-            .frame(width:220)
-            .foregroundStyle(Color.white)
-            .background(Color.mint)
-            .navigationDestination(for: String.self) { str in
-                Text("the sound of \(str)...")
-                    .background(Color.yellow)
+        
+                }
+                
+                //  .navigationTitle("The Music App")
+                .background(Color.blue)
+                .frame(width:330)
+                .foregroundStyle(Color.white)
+                .background(Color.mint)
+                .navigationBarTitle("The Music Zone")
+                .navigationBarItems(trailing: EditButton())
+                .toolbarBackground(Color.mint, for: .navigationBar)
+                .toolbarBackground(.visible, for: .navigationBar)
+                .navigationDestination(for: String.self) { str in
+                    Text("the sound of \(str)...")
+                        .background(Color.yellow)
+                }
             }
         }
-    }
-
-    @State private var player: AVAudioPlayer? = nil
-}
-
-struct Page1_Previews: PreviewProvider {
-    static var previews: some View {
-        PlayingAudio()
+        
+        @State private var player: AVAudioPlayer? = nil
+    
     }
     
-}
+    struct Page1_Previews: PreviewProvider {
+        static var previews: some View {
+            PlayingAudio()
+        }
+        
+    }
+    
+    
+    #Preview {
+        PlayingAudio()
+    }
 
-
-#Preview {
-    PlayingAudio()
-}
--- WEEK 4/WEEK 4/WEEK_4App.swift
+++ WEEK 4/WEEK 4/WEEK_4App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct WEEK_4App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            PlayingAudio()
         }
     }
 }
