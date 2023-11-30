37b1efeec6a0ec3c07c36ba0016c52573312973e
kgcummings <113394583+kgcummings@users.noreply.github.com>
2023-11-30 00:53:26 -0500

    Week 11 Update

--- WEEK 9 FINALPROJ/WEEK 9 FINALPROJ/ContentView.swift
+++ WEEK 9 FINALPROJ/WEEK 9 FINALPROJ/ContentView.swift
@@ -7,24 +7,66 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var springScore = 0
+    @State private var summerScore = 0
+    @State private var fallScore = 0
+    @State private var winterScore = 0
+    
     var body: some View {
         ZStack{
             VStack(spacing: 0){
-              //  Color.pink
                 LinearGradient(stops: [
                     Gradient.Stop(color:.white, location: 0.20),
                     Gradient.Stop(color: .blue, location: 0.75)], startPoint: .top, endPoint: .bottom)
-               // Color.yellow
             }
-               // .frame(width:200, height:300)
-            Text("Bleh")
-                .foregroundStyle(.secondary)
-                .padding(100)
-                .background(.ultraThinMaterial)
+            VStack{
+                Image(systemName: "leaf.fill")
+
+                Button("Cozy up by a fire!", action: fallAnswer)
+                    .buttonStyle(.borderedProminent)
+                    .tint(.green)
+                Button("Pick some flowers...", action: springAnswer)
+                    .buttonStyle(.borderedProminent)
+                    .tint(.blue)
+                Button("Get a tan!", action: summerAnswer)
+                    .buttonStyle(.borderedProminent)
+                    .tint(.orange)
+                Button("Drink a hot beverage.", action: winterAnswer)
+                    .buttonStyle(.borderedProminent)
+                    .tint(.pink)
+            }
+            
         }
         .ignoresSafeArea()
+        
     }
+
+
+    func springAnswer() {
+           print("Spring!")
+        springScore += 1
+        print(springScore)
+       }
+    
+    func summerAnswer() {
+           print("Summer!")
+        summerScore += 1
+        print(summerScore)
+       }
+    
+    func fallAnswer() {
+           print("Fall!")
+        fallScore += 1
+        print(fallScore)
+       }
+
+    func winterAnswer() {
+           print("Winter!")
+        winterScore += 1
+        print(winterScore)
+       }
 }
 
 #Preview {
